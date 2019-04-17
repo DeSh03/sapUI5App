b398fe7a36f2defcd454beaf04a3ed13cdc17a65
Sebastian Dziekonski <sebastian.dziekonski@capgemini.com>
2019-04-17 11:50:37 +0000

    formatMapUrl function added

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -32,6 +32,19 @@ sap.ui.define(["sap/ui/core/format/NumberFormat"], function () {
 			};
 			var oFloatFormat = sap.ui.core.format.NumberFormat.getFloatInstance(oFormatOptions);
 			return oFloatFormat.format(sValue);
-		}
+		},
+			 /**
+	      * Formats an address to a static google maps image
+	      * @public
+	      * @param {string} sBuilding the building number
+	      * @param {string} sStreet the street
+	      * @param {string} sZIP the postal code
+	      * @param {string} sCity the city
+	      * @returns {string} sValue a google maps URL that can be bound to an image
+	      */
+	    formatMapUrl: function(sBuilding, sStreet, sZIP, sCity) {
+	      return "/GoogleMaps/staticmap?zoom=16&size=400x400&markers="
+	        + jQuery.sap.encodeURL(sBuilding +" "+ sStreet + ", " + sZIP +  " " + sCity ) + "&key=AIzaSyCLcbAyetKV-L7f6JagaQZ9HCeAT4shlUM";
+	    }
 	};
 });
